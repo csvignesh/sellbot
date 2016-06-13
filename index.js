66337570d4e0e42970d181cdf48e424445e366ff
vics <vics@ebay.com>
2016-06-13 10:40:20 -0700

    webhook code - post integ

--- index.js
+++ index.js
@@ -27,6 +27,45 @@ app.get('/webhook', function (req, res) {
     }
 });
 
+app.post('/webhook', function (req, res) {
+    var data = req.body;
+
+    // Make sure this is a page subscription
+    if (data.object == 'page') {
+        // Iterate over each entry
+        // There may be multiple if batched
+        data.entry.forEach(function(pageEntry) {
+            var pageID = pageEntry.id;
+            var timeOfEvent = pageEntry.time;
+
+            // Iterate over each messaging event
+            pageEntry.messaging.forEach(function(messagingEvent) {
+                if (messagingEvent.optin) {
+                    //receivedAuthentication(messagingEvent);
+                    res.json(messagingEvent);
+                } else if (messagingEvent.message) {
+                    //receivedMessage(messagingEvent);
+                    res.json(messagingEvent);
+                } else if (messagingEvent.delivery) {
+                    //receivedDeliveryConfirmation(messagingEvent);
+                    res.json(messagingEvent);
+                } else if (messagingEvent.postback) {
+                    //receivedPostback(messagingEvent);
+                    res.json(messagingEvent);
+                } else {
+                    console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+                }
+            });
+        });
+
+        // Assume all went well.
+        //
+        // You must send back a 200, within 20 seconds, to let us know you've
+        // successfully received the callback. Otherwise, the request will time out.
+        res.sendStatus(200);
+    }
+});
+
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
