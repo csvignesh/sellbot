fcce1dc1ee3508391ec81346e0e4eeeb634b19c0
vics <vics@ebay.com>
2016-06-20 15:02:08 -0700

    text msg

--- messenger.js
+++ messenger.js
@@ -169,6 +169,27 @@ app.get('/test', (req, res) => {
     });
 });
 
+function receivedAuthentication(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfAuth = event.timestamp;
+
+    // The 'ref' field is set in the 'Send to Messenger' plugin, in the 'data-ref'
+    // The developer can set this to an arbitrary value to associate the
+    // authentication callback with the 'Send to Messenger' click event. This is
+    // a way to do account linking when the user clicks the 'Send to Messenger'
+    // plugin.
+    var passThroughParam = event.optin.ref;
+
+    console.log("Received authentication for user %d and page %d with pass " +
+        "through param '%s' at %d", senderID, recipientID, passThroughParam,
+        timeOfAuth);
+
+    // When an authentication is received, we'll send a message back to the sender
+    // to let them know it was successful.
+    sendTextMessage(senderID, "Authentication successful");
+}
+
 function sendTextMessage(recipientId, messageText) {
     var messageData = {
         recipient: {
@@ -230,6 +251,8 @@ app.post('/fb', (req, res) => {
             pageEntry.messaging.forEach(function(messagingEvent) {
                 if (messagingEvent.message) {
                     receivedMessage(messagingEvent);
+                } else if (messagingEvent.optin) {
+                    receivedAuthentication(messagingEvent);
                 }
             });
         });
