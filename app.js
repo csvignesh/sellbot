11aa79d9abfdcd53330ca823642285b6c9ff5498
vics <vics@ebay.com>
2016-06-20 15:06:39 -0700

    changed app

--- app.js
+++ app.js
@@ -10,8 +10,6 @@
 /* jshint node: true, devel: true */
 'use strict';
 
-const Wit = require('./wit').wit;
-
 const 
   bodyParser = require('body-parser'),
   config = require('config'),
@@ -485,111 +483,6 @@ function callSendAPI(messageData) {
     }
   });  
 }
-
-
-/* wit code */
-
-const fbMessage = (recipientId, msg, cb) => {
-  const opts = {
-    form: {
-      recipient: {
-        id: recipientId,
-      },
-      message: {
-        text: msg,
-      },
-    },
-  };
-  fbReq(opts, (err, resp, data) => {
-    if (cb) {
-      cb(err || data.error && data.error.message, data);
-    }
-  });
-};
-
-// See the Webhook reference
-// https://developers.facebook.com/docs/messenger-platform/webhook-reference
-const getFirstMessagingEntry = (body) => {
-  const val = body.object == 'page' &&
-          body.entry &&
-          Array.isArray(body.entry) &&
-          body.entry.length > 0 &&
-          body.entry[0] &&
-          body.entry[0].id === FB_PAGE_ID &&
-          body.entry[0].messaging &&
-          Array.isArray(body.entry[0].messaging) &&
-          body.entry[0].messaging.length > 0 &&
-          body.entry[0].messaging[0]
-      ;
-  return val || null;
-};
-
-// Wit.ai bot specific code
-
-// This will contain all user sessions.
-// Each session has an entry:
-// sessionId -> {fbid: facebookUserId, context: sessionState}
-const sessions = {};
-
-const findOrCreateSession = (fbid) => {
-  let sessionId;
-  // Let's see if we already have a session for the user fbid
-  Object.keys(sessions).forEach(k => {
-    if (sessions[k].fbid === fbid) {
-    // Yep, got it!
-    sessionId = k;
-  }
-});
-  if (!sessionId) {
-    // No session found for user fbid, let's create a new one
-    sessionId = new Date().toISOString();
-    sessions[sessionId] = {fbid: fbid, context: {}};
-  }
-  return sessionId;
-};
-
-// Our bot actions
-const actions = {
-  say(sessionId, context, message, cb) {
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to
-    const recipientId = sessions[sessionId].fbid;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      fbMessage(recipientId, message, (err, data) => {
-        if (err) {
-          console.log(
-              'Oops! An error occurred while forwarding the response to',
-              recipientId,
-              ':',
-              err
-          );
-        }
-
-        // Let's give the wheel back to our bot
-        cb();
-    });
-    } else {
-      console.log('Oops! Couldn\'t find user for session:', sessionId);
-      // Giving the wheel back to our bot
-      cb();
-    }
-  },
-  merge(sessionId, context, entities, message, cb) {
-    cb(context);
-  },
-  error(sessionId, context, error) {
-    console.log(error.message);
-  }
-  // You should implement your custom actions here
-  // See https://wit.ai/docs/quickstart
-};
-
-// Setting up our bot
-const wit = new Wit(config.get('witToken'), actions);
-
-/* wit code ends */
 
 // Start server
 // Webhooks must be available via SSL with a certificate signed by a valid 
