b6bcbccbd7448bf86202c39007fd8b2da0862cc4
vics <vics@ebay.com>
2016-06-21 00:24:20 -0700

    wit ai integ

--- app.js
+++ app.js
@@ -226,7 +226,7 @@ app.post('/webhook', function (req, res) {
         } else if (messagingEvent.message) {
           //runWit(event.message.text, (context) => {
           setTimeout(() => {
-            sendTextMessage(sender, JSON.stringify(messagingEvent.message), sessionId);
+            sendTextMessage(sender, messagingEvent.message.text, sessionId);
             res.sendStatus(200);
           }, 2000);
           //});
