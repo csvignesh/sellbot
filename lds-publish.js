5a4a60212751b8c0640f2f1dccc1a52a51835abb
vics <vics@ebay.com>
2016-06-23 22:15:34 -0700

    fix

--- lds-publish.js
+++ lds-publish.js
@@ -1,36 +1,34 @@
 var request = require("request");
 
-module.exports = {
-    publish: function(data) {
-        var formData = {
-            "price": data.price,
-            "title": data.title,
-            "pictureUrl": data.imgUrl,
-            "condition": data.aspectsMap.selected.Condition.indexOf('new') > 0 ? 1000 : 3000,
-            "category": data.leafCaty,
-            "itemSpecific": [],
-            "description": data.desc
-        };
+module.exports = function(data, cb) {
+    var formData = {
+        "price": data.price,
+        "title": data.title,
+        "pictureUrl": data.imgUrl,
+        "condition": data.aspectsMap.selected.Condition.indexOf('new') > 0 ? 1000 : 3000,
+        "category": data.leafCaty,
+        "itemSpecific": [],
+        "description": data.desc
+    };
 
-        delete data.aspectsMap.selected.Condition;
+    delete data.aspectsMap.selected.Condition;
 
-        var keys = Object.keys(data.aspectsMap.selected);
+    var keys = Object.keys(data.aspectsMap.selected);
 
-        keys.forEach((aspectName) => {
-            formData.itemSpecific.push({
-                name: aspectName,
-                value: data.aspectsMap.selected[aspectName]
-            });
+    keys.forEach((aspectName) => {
+        formData.itemSpecific.push({
+            name: aspectName,
+            value: data.aspectsMap.selected[aspectName]
         });
+    });
 
-        request.post({url:'http://seewiw.ebay.com/ldscreate', form: formData}, (err, body) => {
-            if (err) {
-                console.error('upload failed: 123');
-                cb(err);
-            } else {
-                console.log('Upload successful!  Server responded with');
-                cb(body);
-            }
-        });
-    }
+    request.post({url:'http://seewiw.ebay.com/ldscreate', form: formData}, (err, body) => {
+        if (err) {
+            console.error('upload failed: 123');
+            cb(err);
+        } else {
+            console.log('Upload successful!  Server responded with');
+            cb(body);
+        }
+    });
 };
